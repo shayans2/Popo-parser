03cd90107efd317a785e4d197ccfc35ffbc926ee
Shayan Bemanian <shayan.bemanian@yahoo.com>
2022-09-15 00:30:06 +0430

    Fancy up the tests

--- __tests__/run.js
+++ __tests__/run.js
@@ -1,11 +1,25 @@
 const { Parser } = require("../src/Parser");
+const assert = require("assert");
+
+const tests = [require("./literals-test")];
 
 const parser = new Parser();
 
-const program = `/*
-Doc
-*/42`;
+const exec = () => {
+  const program = `/*
+    Doc
+    */42`;
 
-const ast = parser.parse(program);
+  const ast = parser.parse(program);
 
-console.log(JSON.stringify(ast, null, 2));
+  console.log(JSON.stringify(ast, null, 2));
+};
+
+const test = (program, expected) => {
+  const ast = parser.parse(program);
+  assert.deepEqual(ast, expected);
+};
+
+tests.forEach((testRun) => testRun(test));
+
+console.log("✅ All assertions passed");
